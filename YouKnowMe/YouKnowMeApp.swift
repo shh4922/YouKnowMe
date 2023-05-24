36b74710c67d9d464ebeb09c29eb94fa2a4ac18d
hyeonho <gusgh4922@gmail.com>
2023-05-24 20:54:31 +0900

    Initial Commit

---
+++ YouKnowMe/YouKnowMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouKnowMeApp.swift
+//  YouKnowMe
+//
+//  Created by Hyeonho on 2023/05/24.
+//
+
+import SwiftUI
+
+@main
+struct YouKnowMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
